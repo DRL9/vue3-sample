a1361f2e0586898e1f20ce945cecb0a80f4fab5d
Yuanjun Zhong <zhuimengzyj@gmail.com>
2022-02-19 19:00:59 +0800

    update

---
+++ src/components/VResizeObserver.vue
@@ -0,0 +1,79 @@
+<template>
+  <div>
+    <div class="box" ref="refBox" :style="style">box</div>
+    <div>
+      <button @click="clickHandler('width')">width +1</button>
+      <button @click="clickHandler('left')">left +1</button>
+      <button @click="clickHandler('borderWidth')">borderWidth +1</button>
+      <button @click="clickHandler('padding')">padding +1</button>
+      <button @click="clickHandler('scale')">scale +0.1</button>
+    </div>
+    <div>
+      history
+      <ul>
+        <li v-for="(h, idx) in histories" :key="idx">{{ h }}</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref } from "@vue/reactivity";
+import { onMounted, onUnmounted } from "@vue/runtime-core";
+export default {
+  setup() {
+    const refBox = ref();
+    const style = ref({
+      width: "100px",
+      borderWidth: "1px",
+      left: "0px",
+      padding: "0px",
+      transform: "scale(1)",
+    });
+    const histories = ref([]);
+    const observer = new ResizeObserver((entries) => {
+      entries.forEach((entry) => {
+        histories.value.push(
+          `width: ${entry.contentRect.width},
+           left: ${entry.contentRect.left},
+           blockSize: ${entry.borderBoxSize[0].blockSize}
+          `
+        );
+      });
+    });
+    onMounted(() => {
+      observer.observe(refBox.value, {
+        box: "border-box",
+      });
+    });
+    onUnmounted(() => {
+      observer.disconnect();
+    });
+
+    let scale = 1;
+    const clickHandler = (type) => {
+      if (type === "scale") {
+        scale += 0.1;
+        style.value.transform = `scale(${scale})`;
+        return;
+      }
+      style.value[type] = parseInt(style.value[type]) + 1 + "px";
+    };
+    return {
+      refBox,
+      style,
+      histories,
+      clickHandler,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.box {
+  position: relative;
+  background-color: #ccc;
+  border: 1px solid red;
+  box-sizing: border-box;
+}
+</style>
--- src/views/TestObserver.vue
+++ src/views/TestObserver.vue
@@ -2,13 +2,15 @@
   <div>
     <VIntersectionObserver />
     <VMutationObserver />
+    <VResizeObserver />
   </div>
 </template>
 
 <script>
 import VIntersectionObserver from "@/components/VIntersectionObserver.vue";
 import VMutationObserver from "@/components/VMutationObserver.vue";
+import VResizeObserver from "@/components/VResizeObserver.vue";
 export default {
-  components: { VIntersectionObserver, VMutationObserver },
+  components: { VIntersectionObserver, VMutationObserver, VResizeObserver },
 };
 </script>
