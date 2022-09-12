cf65b35de3804971f215f1bea8abbc90804800e4
Yuanjun Zhong <937676516@qq.com>
2022-09-12 08:52:18 +0800

    refactor: move to vite

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,19 @@
+/* eslint-env node */
+require("@rushstack/eslint-patch/modern-module-resolution");
+
 module.exports = {
   root: true,
-  env: {
-    node: true,
-  },
-  extends: ["plugin:vue/vue3-essential", "eslint:recommended", "@vue/prettier"],
+  extends: [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "@vue/eslint-config-prettier",
+  ],
   parserOptions: {
-    parser: "babel-eslint",
+    ecmaVersion: "latest",
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "vue/multi-word-component-names": 0,
   },
-  overrides: [
-    {
-      files: [
-        "**/__tests__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)",
-      ],
-      env: {
-        jest: true,
-      },
-    },
-  ],
 };
--- src/views/About.vue
+++ src/views/About.vue
@@ -3,3 +3,15 @@
     <h1>This is an about page</h1>
   </div>
 </template>
+
+<script>
+import { ref, warn } from "vue";
+
+export default {
+  setup() {
+    const count = ref(0);
+    console.log(count);
+    warn("hello world");
+  },
+};
+</script>
